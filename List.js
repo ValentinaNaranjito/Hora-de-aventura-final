e6e37e92f2752181f289adeb51e0b9e3ec9afc80
ValentinaNaranjito <1213valentina2002@gmail.com>
2022-11-23 15:29:53 -0500

    mensaje

--- List.js
+++ List.js
@@ -1,5 +1,5 @@
-const cards =[
-    {id:"bcard", fav: true , image:"imagenes/Bmo.gif", image2:"https://i.pinimg.com/736x/5e/31/88/5e3188314a94a6538c0a77ec1ab1e9fc.jpg" , image3: "https://youtu.be/jJHxd5nt7ww", nombre: "Bmo", description: "Bmo es un aparato electrónico múlti-propósito, pues no solamente es el sistema de consola de videojuegos vivo de Finn y Jake, si no que también es un tomacorriente portátil, computadora, reproductor de música, videocasetera, reproductor de video, editor de video, compañero de cuarto, cámara, reloj despertador, tostadora, linterna, luz estroboscópica, patinador, jugador de fútbol,​​reproductor de cintas, chef, detective y amigo. Aparecen en la escena del título de la canción temática de cada episodio de Hora de Aventura, justo cuando Finn y Jake golpean sus puños juntos. BMO se caracteriza por ser un robot leal, confiable y útil que protege a Finn y Jake." } , 
+/*const cards =[
+    {id:"bcard", fav: true , image:"imagenes/Bmo.gif", image2:"https://i.pinimg.com/736x/5e/31/88/5e3188314a94a6538c0a77ec1ab1e9fc.jpg" , image3: "https://youtu.be/jJHxd5nt7ww", nombre: "Bmo", description: "" } , 
     {id:"jcard" , image:"imagenes/jake.jpeg", fav: true, image2:"https://i.pinimg.com/564x/66/7d/8c/667d8c5ebae19d27de846b754d8357fc.jpg", nombre: "Jake", description: "Jake es el co-protagonista de la serie Hora de Aventura. Él es un perro mágico y compañero constante de Finn que es su mejor amigo y hermano adoptivo. Jake tiene Poderes Elásticos, que le permiten estirar y manipular su cuerpo. Él es el hijo adoptivo de Joshua y Margaret, y que nació de una protuberancia que le creció a Joshua cuando la Criatura Venenosa inyectó un veneno extraño sobre él. Él tiene un hermano llamado Jermaine. Jake es un perro de 34 años con poderes mágicos, aunque no suele actuar con madurez en algunas ocasiones. Tanto él como su novia, Arcoiris, comparten la pasión por tocar la viola o violín. En Jake el Papá, tiene cinco hijos Lluvia-Cornios con ella."}, 
     {id:"fcard" , fav: true,image:"imagenes/finn.jpg" , image2:"https://i.pinimg.com/564x/2c/71/78/2c717858511bd657b92c63eac485b1b1.jpg width = 240px" , nombre: "Finn" , description : "Finn es el protagonista principal de Hora de Aventura y el mejor amigo y hermano adoptivo de Jake. Él es un joven con un gran espíritu aventurero y explorador de la Tierra de Ooo, junto a Jake, sus amigos y conocidos.Él es hijo de Martin Mertens y Minerva Campbell, y nació en las islas, pero tras una serie de hechos termino llegando a la Tierra de Ooo en donde fue adoptado por Joshua y Margaret. Durante mucho tiempo se creyó que Finn era el último humano, pero en ''Islas'' eso se erradicó por completo. Anteriormente estaba enamorado de la Dulce Princesa, pero ella no le correspondió. Luego se hace novio de la Princesa Flama, quien luego rompe con él. Actualmente sale con la Maga Cazadora."} , 
     {id:"pcard", fav: true , image:"imagenes/dulceprincesa.jpg" , image2:"https://i.pinimg.com/564x/f7/73/32/f773323fb10fa35979ecac41748a276a.jpg" , nombre: "Dulce Princesa", description: " Dulce Princesa es una de las personajes principales de Hora de Aventura y la benevolente princesa del Dulce Reino. Ella también es amiga cercana de Finn y Jake, quienes le rinden lealtad. Tiene 827 años cronológicamente, pero ella dice tener entre 18 y 19 años, siendo esta la edad que parece tener. Desde el episodio ''Amor Peligroso'', se vuelve más joven y tiene 13 años al igual que Finn, donde pasa mucho tiempo con él, pero en ''Demasiado Joven'' regresa a su edad del presente. Ha mantenido la misma edad desde hace mucho tiempo como se puede ver en el episodio ''El Pretendiente'', ya que había pretendientes que esperaron hasta 300 años para salir con ella."}, 
@@ -7,7 +7,7 @@ const cards =[
     {id:"scard", fav: false , image:"imagenes/magacazadora.jpg" , nombre: "Maga Cazadora", description: "Lorem Ipsum"},
     
 
-]
+]*/
 const cardsgeneral =[
     {id:"bcard", fav: true , image:"", image2:"https://i.pinimg.com/736x/5e/31/88/5e3188314a94a6538c0a77ec1ab1e9fc.jpg" , nombre: "Rey Helado", description: "es uno de los personajes principales de Hora de Aventura. Al principio estaba considerado como un antagonista principal, sin embargo ha desarrollado una personalidad más neutral, convirtiéndose en un anti-héroe pero con un gran carisma. Él gobernaba el Reino Helado. Él originalmente era un hombre humano llamado Simon Petrikov el cual estaba comprometido con una mujer humana Betty Grof. Sin embargo debido a la corona mágica que el compro a un trabajador al norte de Escandinavia, lo fue transformando en el Rey Helado, enloqueciendo y obteniendo poderes de controlar el hielo y la nieve. Él logro sobrevivir a la Guerra de los Champiñones y durante las secuelas de esta conocio a Marceline a quien cuido y se hizo amigo de ella. Sin embargo debido a su corona se vi obligado a dejarla." } , 
     {id:"jcard" , image:"", fav: true, image2:"https://i.pinimg.com/564x/66/7d/8c/667d8c5ebae19d27de846b754d8357fc.jpg", nombre: "Jake", description: "Jake es el co-protagonista de la serie Hora de Aventura. Él es un perro mágico y compañero constante de Finn que es su mejor amigo y hermano adoptivo. Jake tiene Poderes Elásticos, que le permiten estirar y manipular su cuerpo. Él es el hijo adoptivo de Joshua y Margaret, y que nació de una protuberancia que le creció a Joshua cuando la Criatura Venenosa inyectó un veneno extraño sobre él. Él tiene un hermano llamado Jermaine. Jake es un perro de 34 años con poderes mágicos, aunque no suele actuar con madurez en algunas ocasiones. Tanto él como su novia, Arcoiris, comparten la pasión por tocar la viola o violín. En Jake el Papá, tiene cinco hijos Lluvia-Cornios con ella."}, 
@@ -21,14 +21,26 @@ const users =[
 
 ]
 
-
+/*
+var cards = []
 const request = new XMLHttpRequest();
 request.open('GET', 'https://adventure-time-api.herokuapp.com/api/v1/characters', true);
 request.onload = function(){
     const data = JSON.parse(this.response);
     data.forEach(element => {
-        console.log(element.id);
+        const card = {
+            id: element.id, 
+            fav: true , 
+            image: element.sprite, 
+            image2:element.sprite, 
+            image3: element.sprite,
+             nombre: element.fullName, 
+             description: element.quotes[0]
+        }
+        cards2.push(card)
     });
+    console.log(cards2);
 }
 
 request.send();
+*/
--- index.js
+++ index.js
@@ -1,4 +1,36 @@
+const fetchData = async () => {
+    const url = 'https://adventure-time-api.herokuapp.com/api/v1/characters'
+    // Get the list from the api
+    const data = await fetch(url)
+    // Convert to json
+    const dataJson = await data.json()
 
+    var cards = []
+    dataJson.forEach(element => {
+        const card = {
+            id: element.id, 
+            fav: true , 
+            image: "imagenes/" + element.id + '.jpeg', 
+            image2: "imagenes/" + element.id +'-2'+ '.jpeg', 
+            image3: "imagenes/" + element.id + '.jpeg', 
+             nombre: element.fullName, 
+             description: element.quotes[0]
+        }
+        cards.push(card)
+    });
+    console.log(cards);
+
+    cards.forEach((card, index) => {
+        if (index<3) {
+            divtanda1.innerHTML+=`<a href="personaje.html?id=${card.id}"> <div class="reborde1"> <img src=${card.image} height="198px" width="325px"></div> </a>`
+            tanda1.appendChild(divtanda1)
+        }
+        else{
+            divtanda2.innerHTML+=`<a href="personaje.html?id=${card.id}"> <div class="reborde1"> <img src=${card.image} height="198px" width="325px"></div> </a>`
+            tanda2.appendChild(divtanda2)
+        }
+        })
+}
     
 const tanda1 = document.getElementById("tanda1");
 const tanda2 = document.getElementById("tanda2");
@@ -9,13 +41,4 @@ divtanda1.className = "tanda1"
 let divtanda2 = document.createElement("div")
 divtanda2.className = "tanda2"
 
-cards.forEach((card, index) => {
-if (index<3) {
-    divtanda1.innerHTML+=`<a href="personaje.html?id=${card.id}"> <div class="reborde1"> <img src=${card.image} height="198px" width="325px"></div> </a>`
-    tanda1.appendChild(divtanda1)
-}
-else{
-    divtanda2.innerHTML+=`<a href="personaje.html?id=${card.id}"> <div class="reborde1"> <img src=${card.image} height="198px" width="325px"></div> </a>`
-    tanda2.appendChild(divtanda2)
-}
-})
+fetchData()
